3635a59d677149fca76fde91e6c7cf5af6994fbb
Stephen2697 <c16377163@mydit.ie>
2019-04-08 12:28:47 +0100

    Initial Commit

---
+++ communityLife/communityLife/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  communityLife
+//
+//  Created by Stephen Alger on 08/04/2019.
+//  Copyright © 2019 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ communityLife/communityLife/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  communityLife
+//
+//  Created by Stephen Alger on 08/04/2019.
+//  Copyright © 2019 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
