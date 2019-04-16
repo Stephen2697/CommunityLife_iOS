1d12290a257ec0ddf56526e8b8a7d9aa93f695d3
Stephen2697 <c16377163@mydit.ie>
2019-04-16 17:20:08 +0100

    Push/Pop Segue from NextEvent collectionview to event page
    User can now select an event in their "next events" section and get to the page details quicker

--- communityLife/communityLife/MyEventsTableVC.swift
+++ communityLife/communityLife/MyEventsTableVC.swift
@@ -152,45 +152,47 @@ class MyEventsTableVC: UITableViewController {
         let photo8 = UIImage(named: "EltonJ")
         let photo9 = UIImage(named: "TheEagles")
         
+        
+        
         //create sample dates
         let newDateOne = "01/06/2019"
         let newDateTwo = "07/06/2019"
         let newDateThree = "24/06/2019"
         
-        guard let eventOne = Event(eventDate: newDateOne, locationShort: "Aviva Stadium", locationLong: "Aviva Stadium, Dublin", time: "8.30pm", descriptionString: "Ireland vs Gibraltar", eventImage: photo1, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 1, topicName: "Soccer", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")!  ) else {
+        guard let eventOne = Event(eventDate: newDateOne, locationShort: "Aviva Stadium", locationLong: "Aviva Stadium, Dublin", time: "8.30pm", descriptionString: "Ireland vs Gibraltar", eventImage: photo1, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 1, topicName: "Soccer", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black  ) else {
             fatalError("Unable to instantiate eventOne")
         }
         
-        guard let eventSeven = Event(eventDate: newDateOne, locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "8.00pm", descriptionString: "Rolling Stones Concert", eventImage: photo7, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")!  ) else {
+        guard let eventSeven = Event(eventDate: newDateOne, locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "8.00pm", descriptionString: "Rolling Stones Concert", eventImage: photo7, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black  ) else {
             fatalError("Unable to instantiate eventSeven")
         }
         
-        guard let eventFour = Event(eventDate: newDateOne, locationShort: "Thomond Park", locationLong: "Thomond Park, Limerick", time: "5.30pm", descriptionString: "Munster vs Ospreys", eventImage: photo6, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2, topicName: "Rugby",needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")! ) else {
+        guard let eventFour = Event(eventDate: newDateOne, locationShort: "Thomond Park", locationLong: "Thomond Park, Limerick", time: "5.30pm", descriptionString: "Munster vs Ospreys", eventImage: photo6, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2, topicName: "Rugby",needsTickets: true,startColor: UIColor.black, endColor: UIColor.black ) else {
             fatalError("Unable to instantiate eventFour")
         }
         
-        guard let eventTwo = Event(eventDate: newDateTwo, locationShort: "UCD Campus", locationLong: "UCD Campus, Dublin", time: "1.30pm", descriptionString: "UCD vs Shamrock Rovers", eventImage: photo2, attendingBool: false, wheelBool: true, toiletFacBool: false, parkBool: true, topicID: 1,topicName: "Soccer", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")!  ) else {
+        guard let eventTwo = Event(eventDate: newDateTwo, locationShort: "UCD Campus", locationLong: "UCD Campus, Dublin", time: "1.30pm", descriptionString: "UCD vs Shamrock Rovers", eventImage: photo2, attendingBool: false, wheelBool: true, toiletFacBool: false, parkBool: true, topicID: 1,topicName: "Soccer", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black  ) else {
             fatalError("Unable to instantiate eventTwo")
         }
         
-        guard let eventFive = Event(eventDate: newDateTwo, locationShort: "UCD Campus", locationLong: "UCD Campus, Dublin", time: "1.00pm", descriptionString: "Dundrum Special Olympics Blitz", eventImage: photo3, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 1,topicName: "Sports", needsTickets: false,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")! ) else {
+        guard let eventFive = Event(eventDate: newDateTwo, locationShort: "UCD Campus", locationLong: "UCD Campus, Dublin", time: "1.00pm", descriptionString: "Dundrum Special Olympics Blitz", eventImage: photo3, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 1,topicName: "Sports", needsTickets: false,startColor: UIColor.black, endColor: UIColor.black ) else {
             fatalError("Unable to instantiate eventFive")
         }
         
-        guard let eventEight = Event(eventDate: newDateTwo, locationShort: "Aviva Stadium", locationLong: "Aviva Stadium, Dublin", time: "4.00pm", descriptionString: "Ireland vs New Zealand", eventImage: photo5, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2,topicName: "Rugby", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")!  ) else {
+        guard let eventEight = Event(eventDate: newDateTwo, locationShort: "Aviva Stadium", locationLong: "Aviva Stadium, Dublin", time: "4.00pm", descriptionString: "Ireland vs New Zealand", eventImage: photo5, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2,topicName: "Rugby", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black  ) else {
             fatalError("Unable to instantiate eventEight")
         }
         
-        guard let eventThree = Event(eventDate: newDateThree, locationShort: "RDS Donnybrook", locationLong: "RDS Donnybrook, Dublin", time: "2.30pm", descriptionString: "Leinster vs Edinburgh", eventImage: photo4, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2, topicName: "Rugby", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")! ) else {
+        guard let eventThree = Event(eventDate: newDateThree, locationShort: "RDS Donnybrook", locationLong: "RDS Donnybrook, Dublin", time: "2.30pm", descriptionString: "Leinster vs Edinburgh", eventImage: photo4, attendingBool: false, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 2, topicName: "Rugby", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black ) else {
             fatalError("Unable to instantiate eventThree")
         }
         
         
-        guard let eventNine = Event(eventDate: newDateThree, locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "6.30pm", descriptionString: "The Eagles Concert", eventImage: photo9, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")!  ) else {
+        guard let eventNine = Event(eventDate: newDateThree, locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "6.30pm", descriptionString: "The Eagles Concert", eventImage: photo9, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black  ) else {
             fatalError("Unable to instantiate eventNine")
         }
         
-        guard let eventSix = Event(eventDate: "20/07/2019", locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "7.30pm", descriptionString: "Elton John Concert", eventImage: photo8, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor(named: "lightBlue")!, endColor: UIColor(named: "lightPurple")! ) else {
+        guard let eventSix = Event(eventDate: "20/07/2019", locationShort: "3Arena", locationLong: "3Arena, Dublin", time: "7.30pm", descriptionString: "Elton John Concert", eventImage: photo8, attendingBool: true, wheelBool: true, toiletFacBool: true, parkBool: true, topicID: 3,topicName: "Music", needsTickets: true,startColor: UIColor.black, endColor: UIColor.black ) else {
             fatalError("Unable to instantiate eventNine")
         }
         
--- communityLife/communityLife/NextEventsViewController.swift
+++ communityLife/communityLife/NextEventsViewController.swift
@@ -33,6 +33,43 @@ class NextEventsViewController: UIViewController {
         collectionView?.dataSource = self
         collectionView?.delegate = self
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        //Creating a switch statement which deals with the two kinds of segues which will be encountered when using the same scene for two separate purposes... In the first case we wish to add new food items, in the second we want to edit food items. To handle these similar activities we will have to make slight adjustments to the scene based on the activity we wish to carry out
+        switch(segue.identifier ?? "")
+        {
+            
+            
+        //Show item details
+        case "ShowDetailNextEvent":
+            //constant takes the users aimed destination
+            guard let EventVC = segue.destination as? EventVC else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            //save the sending table cell so we can display the corresponding food item details
+            guard let selectedItemCell = sender as? NextEventCollectionViewCell else {
+                fatalError("Unexpected sender: \(String(describing: sender))")
+            }
+            
+            //save the index path of the given cell
+            guard let indexPath = collectionView.indexPath(for: selectedItemCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            //take in object selected
+            let selectedItem = eventItems[indexPath.row]
+            EventVC.ToDisplayEventItem = selectedItem
+            
+        //default case - will never get here...hopefully!
+        default:
+            fatalError("Invalid Segue Identifier Calling: \(String(describing: segue.identifier))")
+        }
+    }
 }
 
 
